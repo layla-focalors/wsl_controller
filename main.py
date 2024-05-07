022b526bae056e7c94160358269dfe05e18b8517
layla-focalors <starlights2283@gmail.com>
2024-05-07 16:21:00 +0900

    add System UI

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
 import flet as ft
 import modules.update as upd
 
+def deploy_page():
+    return ft.Text("Deploy Page")
+
+def environment_page():
+    return ft.Text("Environment Page")
+
+def functions_page():
+    return ft.Text("Functions Page")
+
+def storages_page():
+    return ft.Text("Storages Page")
+
+def settings_page():
+    return ft.Text("Settings Page")
+
 def main(page: ft.Page):
     page.window_width = 1280
     page.window_height = 720
@@ -14,13 +29,15 @@ def main(page: ft.Page):
     else:
         pass
     
+    body = ft.Column([ft.Text("Body!")], alignment=ft.MainAxisAlignment.START, expand=True)
+    
     rail = ft.NavigationRail(
         selected_index=0,
         label_type=ft.NavigationRailLabelType.ALL,
         # extended=True,
         min_width=100,
         min_extended_width=400,
-        leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Deploy"),
+        leading=ft.FloatingActionButton(icon=ft.icons.CREATE, text="Deploy", on_click=lambda e: update_body(-1, body)),
         group_alignment=-0.9,
         destinations=[
             ft.NavigationRailDestination(
@@ -42,7 +59,7 @@ def main(page: ft.Page):
                 label_content=ft.Text("Settings"),
             ),
         ],
-        on_change=lambda e: print("Selected destination:", e.control.selected_index),
+        on_change=lambda e: update_body(e.control.selected_index, body),
     )
 
     page.add(
@@ -50,11 +67,28 @@ def main(page: ft.Page):
             [
                 rail,
                 ft.VerticalDivider(width=1),
-                ft.Column([ ft.Text("Body!")], alignment=ft.MainAxisAlignment.START, expand=True),
+                body,
             ],
             expand=True,
         )
     )
 
+    def update_body(index, body):
+        body.controls.clear()
+        if index == -1:
+            body.controls.append(deploy_page())
+        elif index == 0:
+            body.controls.append(environment_page())
+        elif index == 1:
+            body.controls.append(functions_page())
+        elif index == 2:
+            body.controls.append(storages_page())
+        elif index == 3:
+            body.controls.append(settings_page())
+        page.update()
+
+    update_body(0, body)
+
+    page.update()
 
 ft.app(main)
